0c6da9eb7d65bde6f73d8d8c221898b37095cc77
Sam Morris <samcmorris1@gmail.com>
2023-04-13 10:53:31 +0100

    simplify command line args

--- src/fastmatrix.cpp
+++ src/fastmatrix.cpp
@@ -16,41 +16,23 @@ int main(int argc, char *argv[]) {
   try {
     TCLAP::CmdLine cmd("Command description message", ' ', "0.1");
 
-    TCLAP::ValueArg<std::string> chromosomesToAnalyseArg(
-        "c", "chromosomes", "comma seperated list of chromosomes you want to analyse", true, "homer", "string");
-    cmd.add(chromosomesToAnalyseArg);
-
-    TCLAP::ValueArg<std::string> prefixArg("p", "prefix", "prefix of filenames you want to analuse", true, "homer",
-                                           "string");
-    cmd.add(prefixArg);
-
     TCLAP::ValueArg<std::string> outputArg("o", "output", "Full output file name - no extension will be added", true,
                                            "homer", "string");
     cmd.add(outputArg);
 
-    TCLAP::ValueArg<std::string> fileType("f", "filetype",
-                                          "File type - e.g. chunkcounts or chunklengths - program will "
-                                          "automatically detect gzipped files",
-                                          true, "homer", "string");
-    cmd.add(fileType);
-
-    // TCLAP::SwitchArg normaliseSwitch("a","normalise","normalise the copying
-    // matrix before printing (i.e. divide each row by its sum", cmd, false);
-    // cmd.add(normaliseSwitch);
+    TCLAP::ValueArg<std::string> fileList("f", "filelist", "List of files to merge, seperated by commas", "true", "homer", "string");
+    cmd.add(fileList);
 
     cmd.parse(argc, argv);
 
     // assign arguments to variables
-    std::string chromosomesToAnalyseString = chromosomesToAnalyseArg.getValue();
     std::string output = outputArg.getValue();
-    std::string prefix = prefixArg.getValue();
-    std::string filetype = fileType.getValue();
-
-    std::vector chromosomesToAnalyseVector = split_string_to_vector(chromosomesToAnalyseString, ',');
+    
+    std::vector filesToAnalyseVector = split_string_to_vector(fileList, ',');
 
     // run some checks here //
 
-    int n_chr = chromosomesToAnalyseVector.size();
+    int n_chr = filesToAnalyseVector.size();
 
     // no point summing chromosomes when there's only one!
     if (n_chr == 1) {
@@ -62,7 +44,8 @@ int main(int argc, char *argv[]) {
 
     // now we want to read in the first file and use that as a base //
 
-    std::string firstFilePath = prefix + chromosomesToAnalyseVector[0] + '.' + filetype;
+    std::string firstFilePath = filesToAnalyseVector[0];
+    
     std::cout << "first path is: " << firstFilePath << "\n";
 
     const char *firstFilePathChar = firstFilePath.c_str();
@@ -85,11 +68,11 @@ int main(int argc, char *argv[]) {
     int ff_cols = firstFile.cols();
 
     // remove first element from the vector - we don't need any more
-    chromosomesToAnalyseVector.erase(chromosomesToAnalyseVector.begin());
+    filesToAnalyseVector.erase(filesToAnalyseVector.begin());
 
-    for (std::string i : chromosomesToAnalyseVector) {
+    for (std::string i : filesToAnalyseVector) {
 
-      std::string infile = prefix + i + '.' + filetype;
+      std::string infile = i;
       const char *infilechar = infile.c_str();
 
       std::cout << "Processing infile: " << infile << "\n";
